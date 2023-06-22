62190333a435b3d4b62fbf1add130c0e29e9a018
whydylan <111309669+whydylan@users.noreply.github.com>
2023-06-22 16:20:24 +0100

    Create auto-bull-riding.lua

---
+++ Da Hood/auto-bull-riding.lua
@@ -0,0 +1,40 @@
+local Connections, Held = {}, {['Up'] = false, ['Down'] = false, ['Left'] = false, ['Right'] = false}
+
+local Services = {
+    Players = game:GetService("Players")
+}
+
+local Information = {
+    Mounted = false,
+    Mapping = {
+        ['Up'] = 0x57, ['Down'] = 0x53, ['Left'] = 0x41, ['Right'] = 0x44
+    }
+}
+
+local Settings = {
+    Enabled = true
+}
+
+local GUI = Services['Players'].LocalPlayer.PlayerGui:WaitForChild('MainScreenGui').BullControls
+
+while task.wait() do
+    Information.Mounted = (Settings.Enabled and GUI.Visible)
+    
+    if Information.Mounted then
+        for index, item in pairs(GUI:GetChildren()) do
+            task.spawn(function()
+                if item.ImageColor3 == Color3.fromRGB(255, 0, 0) and Held[item.Name] == false then
+                    rconsoleprint('Pressing: ' .. item.Name .. "\n")
+                    
+                    Held[item.Name] = true
+                    keypress(Information.Mapping[item.Name])
+
+                    repeat task.wait() until item.ImageColor3 ~= Color3.fromRGB(255, 0, 0)
+
+                    keyrelease(Information.Mapping[item.Name])
+                    Held[item.Name] = false
+                end
+            end)
+        end
+    end
+end
